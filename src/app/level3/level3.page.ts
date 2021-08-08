38ba55cb98325a807e75f135e242a324874ff329
umang39 <toshugupta579@gmail.com>
2021-08-08 10:57:55 +0530

    ui updated 2
    Signed-off-by: umang39 <toshugupta579@gmail.com>

--- src/app/level3/level3.page.ts
+++ src/app/level3/level3.page.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SharedDataService } from '../providers/shared-data/shared-data.service';
-
+import { timer } from 'rxjs';
 @Component({
   selector: 'app-level3',
   templateUrl: './level3.page.html',
   styleUrls: ['./level3.page.scss'],
 })
 export class Level3Page implements OnInit {
-
+  time : Number
   currentView: number;
   Instructions = 0;
   showCardDetails = 1;
   inputTransactionDetails = 2;
   inputOTP = 3;
-  
+  interval : any
+  count = 0 
 
   constructor(public shared: SharedDataService) {
     this.currentView = this.Instructions;
@@ -37,8 +38,14 @@ export class Level3Page implements OnInit {
   pay() {
     console.log("Payment is initiated!");
     this.currentView = this.inputOTP;
+    this.time = 59
+    this.setTime()
   }
 
+ 
+  async setTime(){
+    //sarthak handle this situation.....
+  }
   checkOTP() {
     console.log("Verifying transaction!!");
     //this.currentView = this.inputOTP;
