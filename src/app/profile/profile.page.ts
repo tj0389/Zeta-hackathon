4c6daa9317913da7a7101eadd780fec5b4fab8a1
umang39 <toshugupta579@gmail.com>
2021-08-26 18:10:39 +0530

    profile added
    Signed-off-by: umang39 <toshugupta579@gmail.com>

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -8,8 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class ProfilePage implements OnInit {
 
   constructor() { }
-
+  flag = "male";
+  first = ""
+  last = ""
+  mobile
+  pan 
+  dob
   ngOnInit() {
   }
-
+  changerole(){ 
+   
+    
+      if(this.flag == "male"){
+        this.flag = "female"
+      }
+      else{
+        this.flag = "male"
+      }
+  }
+  submitHandler(){
+    let  data = {
+      firstname : this.first,
+      lastname : this.last,
+      pancard : this.pan,
+      mobile : this.mobile,
+      dob : this.dob.substring(0,10),
+      gender : this.flag,
+    }
+    console.log(data)
+  }
 }
