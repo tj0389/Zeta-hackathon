74796db20967817a8ca2f75b338293399a4e31ab
Tanmay Jain <jaintanmay389@gmail.com>
2021-08-26 23:06:09 +0530

    updated level 3

--- src/app/level3/level3.page.ts
+++ src/app/level3/level3.page.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../providers/auth/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { NavController, MenuController, AlertController, LoadingController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
@@ -18,7 +19,7 @@ export class Level3Page implements OnInit {
   amount;
   otp;
 
-  constructor(public shared: SharedDataService,public navCtrl:NavController,private activatedRoute:ActivatedRoute,private menuCtrl:MenuController,public alertCtrl:AlertController,public loadingCtrl:LoadingController) {
+  constructor(public shared: SharedDataService,public navCtrl:NavController,private activatedRoute:ActivatedRoute,private menuCtrl:MenuController,public alertCtrl:AlertController,public loadingCtrl:LoadingController,private authService:AuthService) {
 
     this.shared.otptime=this.shared.otp_time;
 
@@ -107,5 +108,92 @@ export class Level3Page implements OnInit {
     this.navCtrl.navigateRoot('level3');
     this.shared.is_transaction_complete=false;
     this.shared.savescore(1);
+  }
+
+  setup(){
+   this.createAccountHolder();
+   this.issueBundels();
+  }
+  
+  createAccountHolder(){
+    let data={
+      "ifiID": "140793",
+      "formID": "Caption-12345",
+      "applicationType": "CREATE_ACCOUNT_HOLDER",
+      "spoolID": "3deb5a70-311c-11ea-978f-2e728ce88125",
+      "individualType": "REAL",
+      "salutation": "",
+      "firstName": "<<Firstname>",
+      "middleName": "",
+      "lastName": "<<lastname>>",
+      "profilePicURL": "",
+      "dob": {
+          "year": 1985,
+          "month": 11,
+          "day": 20
+      },
+      "gender": "",
+      "mothersMaidenName": "",
+      "kycDetails": {
+          "kycStatus": "MINIMAL",
+          "kycStatusPostExpiry": "KYC_EXPIRED",
+          "kycAttributes": {},
+          "authData": {
+              "PAN": "<<pan number>>"
+          },
+          "authType": "PAN"
+      },
+      "vectors": [
+          {
+              "type": "p",
+              "value": "+91<<mobilenumber>>",
+              "isVerified": true
+          }
+      ],
+      "pops": [],
+      "customFields": {
+        "entity_id": "ABCD0001"
+      }
+    }
+    // this.authService.getzetaapi('resources/461c368b-00eb-46ec-9321-a0c29598ed7a')
+    this.authService.postzataapi(JSON.stringify(data),'applications/newIndividual').then(async (result) => {
+      console.log(result);
+      // await this.loading.dismiss();
+      // individual ID 
+      
+    },async (err) => {
+      // await this.loading.dismiss();
+      console.log(err);
+      const alert = await this.alertCtrl.create({
+        header: 'Error',
+        message: 'Something went wrong. Try again later',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    });
+
+  }
+
+  issueBundels(){
+    let data={
+        "accountHolderID": "8f481b75-afff-4ef2-ab9b-97b3e08fb17c",  // individual id
+        "name": "VBO_bundle_02",
+        "phoneNumber": "+919843543890"         // unique
+    }
+    // this.authService.getzetaapi('resources/461c368b-00eb-46ec-9321-a0c29598ed7a')
+    this.authService.postzataapi(data,'bundles/20c6dcd0-7728-436f-b082-0a0e60baf303/issueBundle').then(async (result) => {
+      console.log(result);
+      // await this.loading.dismiss();
+      
+    },async (err) => {
+      // await this.loading.dismiss();
+      console.log(err);
+      const alert = await this.alertCtrl.create({
+        header: 'Error',
+        message: 'Something went wrong. Try again later',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    });
   }
 }
