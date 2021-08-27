ef98aa62e8cb6e37137841e544d5e6fad622b159
Tanmay Jain <jaintanmay389@gmail.com>
2021-08-27 12:38:09 +0530

    updated

--- src/app/level3/level3.page.ts
+++ src/app/level3/level3.page.ts
@@ -18,6 +18,7 @@ export class Level3Page implements OnInit {
   upiid;
   amount;
   otp;
+  loading: HTMLIonLoadingElement;
 
   constructor(public shared: SharedDataService,public navCtrl:NavController,private activatedRoute:ActivatedRoute,private menuCtrl:MenuController,public alertCtrl:AlertController,public loadingCtrl:LoadingController,private authService:AuthService) {
 
@@ -29,13 +30,16 @@ export class Level3Page implements OnInit {
       this.id=0;
     else
       this.id+=1;
+
+    if (shared.individualID=='')
+      navCtrl.navigateRoot(['level3',{id:1}])
   }
 
   ngOnInit() {
   }
   
   nextpage(id:number){
-    if (id==0){
+    if (id==1){
       if (this.shared.current_level<3){
         this.alertCtrl.create({
           header : `Alert!`,
@@ -109,84 +113,159 @@ export class Level3Page implements OnInit {
     this.shared.is_transaction_complete=false;
     this.shared.savescore(1);
   }
-
+  
   setup(){
-   this.createAccountHolder();
-   this.issueBundels();
+    this.issueBundels();
   }
   
   createAccountHolder(){
-    let data={
-      "ifiID": "140793",
-      "formID": "Caption-12345",
-      "applicationType": "CREATE_ACCOUNT_HOLDER",
-      "spoolID": "3deb5a70-311c-11ea-978f-2e728ce88125",
-      "individualType": "REAL",
-      "salutation": "",
-      "firstName": "<<Firstname>",
-      "middleName": "",
-      "lastName": "<<lastname>>",
-      "profilePicURL": "",
-      "dob": {
-          "year": 1985,
-          "month": 11,
-          "day": 20
-      },
-      "gender": "",
-      "mothersMaidenName": "",
-      "kycDetails": {
-          "kycStatus": "MINIMAL",
-          "kycStatusPostExpiry": "KYC_EXPIRED",
-          "kycAttributes": {},
-          "authData": {
-              "PAN": "<<pan number>>"
-          },
-          "authType": "PAN"
-      },
-      "vectors": [
-          {
-              "type": "p",
-              "value": "+91<<mobilenumber>>",
-              "isVerified": true
-          }
-      ],
-      "pops": [],
-      "customFields": {
-        "entity_id": "ABCD0001"
+      let val=Math.floor(10000 + Math.random() * 90000);
+      console.log(this.shared.user);
+      let data = {
+        "AUTH_TYPE": "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwidGFnIjoiQS10WEJzbmc3MzFCbzdXX3VmTHFRdyIsImFsZyI6IkExMjhHQ01LVyIsIml2IjoibGtRN2l0Ukc3c1FIOWpwbyJ9.cqOfj-TsUgQwSjx2ch3ySm0ph44IQviWqVTfSAPN-A8.7rR5lMRvjRLKq_2hi158gw.H040En8DSMegkWH16maCRSleNW-ZpmGXFVUjZUgT9iuAYPRNlEIzjU1hCk1bnT18ic34tMVkeMMAiskJgOTyeA-627V_5C-y8xGIoSF6c5P9tEsu1cURX55EmFl5A4OAYb3xHJNYpueeWSsTMkyk60fg7ZA1ATr74B0QzEo1zKnjzZxQoZHYayWKlFmi-LSYic3lnDFgV35WEjveSzzPyOZQprTkDh5ee21QbJkER2lZWv5KqQge1ViOhi1UPWa1OjtJyAQGv86W17EQ2wA0_ESFUg3-kahwfhhr5_PxtWc_P698vQ-fFiXL0Qvra_-6erzJcvy4ghUanp5k2xUNkR-nC_tX3lLqvAqL3Ad_PqdDdOPcGdd0qHPSJBDf6lB1.IKEnNxngSfQRGEunXf6b8w",
+        "requestCallType": "POST",
+        "url": "https://fusion.preprod.zeta.in/api/v1/ifi/140793/applications/newIndividual",
+        "ifiID": "140793",
+        "formID": "Caption-111111",
+        "applicationType": "CREATE_ACCOUNT_HOLDER",
+        "spoolID": "3deb5a70-311c-11ea-978f-2e728ce88125",
+        "individualType": "REAL",
+        "salutation": "",
+        "firstName": 'Sarthak',
+        "middleName": "",
+        "lastName": 'Jain',
+        "profilePicURL": "",
+        "dob": {
+            "year": 1985,
+            "month": 11,
+            "day": 20
+        },
+        "gender": "",
+        "mothersMaidenName": "",
+        "kycDetails": {
+            "kycStatus": "MINIMAL",
+            "kycStatusPostExpiry": "KYC_EXPIRED",
+            "kycAttributes": {},
+            "authData": {
+                "PAN": "NNZAA2318A"
+            },
+            "authType": "PAN"
+        },
+        "vectors": [
+            {
+                "type": "p",
+                "value": "+919027765985",
+                "isVerified": true
+            }
+        ],
+        "pops": [],
+        "customFields": {
+          "entity_id": "ABCD0001"
+        }
       }
-    }
-    // this.authService.getzetaapi('resources/461c368b-00eb-46ec-9321-a0c29598ed7a')
-    this.authService.postzataapi(JSON.stringify(data),'applications/newIndividual').then(async (result) => {
-      console.log(result);
-      // await this.loading.dismiss();
-      // individual ID 
-      
-    },async (err) => {
-      // await this.loading.dismiss();
-      console.log(err);
-      const alert = await this.alertCtrl.create({
-        header: 'Error',
-        message: 'Something went wrong. Try again later',
-        buttons: ['OK'],
+      this.showLoader();
+      this.authService.postData(data, 'hitZetaAPI').then(async (result) => {
+        await this.loading.dismiss();
+        console.log(result);
+        if (result['status']=='APPROVED'){
+          this.shared.user.individualID=result['individualID'];
+          // this.issueBundels();
+        }
+        else{
+          const alert = await this.alertCtrl.create({
+            header: result['status'],
+            message: result['statusDetails']['message'],
+            buttons: ['OK'],
+          });
+          await alert.present();
+        }
+      }, async (err) => {
+        await this.loading.dismiss();
+        console.log(err);
+        const alert = await this.alertCtrl.create({
+          header: 'Error',
+          message: 'Something went wrong. Try again later',
+          buttons: ['OK'],
+        });
+        await alert.present();
       });
-      await alert.present();
-    });
-
   }
 
   issueBundels(){
+    // account-id:'4d45a6fe-0048-4cda-bcb0-8d3cc35ed21a'
+    //resource-id:'"945f4865-f0eb-477d-929d-da583cc0439f"
     let data={
-        "accountHolderID": "8f481b75-afff-4ef2-ab9b-97b3e08fb17c",  // individual id
-        "name": "VBO_bundle_02",
-        "phoneNumber": "+919843543890"         // unique
+      "AUTH_TYPE": "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwidGFnIjoiQS10WEJzbmc3MzFCbzdXX3VmTHFRdyIsImFsZyI6IkExMjhHQ01LVyIsIml2IjoibGtRN2l0Ukc3c1FIOWpwbyJ9.cqOfj-TsUgQwSjx2ch3ySm0ph44IQviWqVTfSAPN-A8.7rR5lMRvjRLKq_2hi158gw.H040En8DSMegkWH16maCRSleNW-ZpmGXFVUjZUgT9iuAYPRNlEIzjU1hCk1bnT18ic34tMVkeMMAiskJgOTyeA-627V_5C-y8xGIoSF6c5P9tEsu1cURX55EmFl5A4OAYb3xHJNYpueeWSsTMkyk60fg7ZA1ATr74B0QzEo1zKnjzZxQoZHYayWKlFmi-LSYic3lnDFgV35WEjveSzzPyOZQprTkDh5ee21QbJkER2lZWv5KqQge1ViOhi1UPWa1OjtJyAQGv86W17EQ2wA0_ESFUg3-kahwfhhr5_PxtWc_P698vQ-fFiXL0Qvra_-6erzJcvy4ghUanp5k2xUNkR-nC_tX3lLqvAqL3Ad_PqdDdOPcGdd0qHPSJBDf6lB1.IKEnNxngSfQRGEunXf6b8w",
+      "requestCallType": "POST",
+      "url": "https://fusion.preprod.zeta.in/api/v1/ifi/140793/bundles/20c6dcd0-7728-436f-b082-0a0e60baf303/issueBundle",
+        "accountHolderID":'cfaef7b3-565c-46b8-86ff-7625cace86e9',  // individual id
+        "name": this.shared.user.firstName,
+        "phoneNumber":"+91"+String(this.shared.user.mobile)  // unique
     }
-    // this.authService.getzetaapi('resources/461c368b-00eb-46ec-9321-a0c29598ed7a')
-    this.authService.postzataapi(data,'bundles/20c6dcd0-7728-436f-b082-0a0e60baf303/issueBundle').then(async (result) => {
+    this.showLoader();
+    this.authService.postData(data, 'hitZetaAPI').then(async (result) => {
+      await this.loading.dismiss();
       console.log(result);
-      // await this.loading.dismiss();
-      
-    },async (err) => {
-      // await this.loading.dismiss();
+      if (result['accounts']!=undefined && result['paymentInstruments']!=undefined){
+        this.shared.user.accountID=result['accounts'][0]['accountID'];
+        this.shared.user.resourceID=result['paymentInstruments'][0]['resourceID'];
+        this.addmoney();
+      }
+      else{
+        const alert = await this.alertCtrl.create({
+          header: 'Error',
+          message: 'Something went wrong. Try again later',
+          buttons: ['OK'],
+        });
+        await alert.present();
+      }
+    }, async (err) => {
+      await this.loading.dismiss();
+      const alert = await this.alertCtrl.create({
+        header: 'Error',
+        message: 'Something went wrong. Try again later',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    });
+  }
+
+  addmoney(){
+    let val=Math.floor(10000 + Math.random() * 90000);
+    let data={
+      "AUTH_TYPE": "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwidGFnIjoiQS10WEJzbmc3MzFCbzdXX3VmTHFRdyIsImFsZyI6IkExMjhHQ01LVyIsIml2IjoibGtRN2l0Ukc3c1FIOWpwbyJ9.cqOfj-TsUgQwSjx2ch3ySm0ph44IQviWqVTfSAPN-A8.7rR5lMRvjRLKq_2hi158gw.H040En8DSMegkWH16maCRSleNW-ZpmGXFVUjZUgT9iuAYPRNlEIzjU1hCk1bnT18ic34tMVkeMMAiskJgOTyeA-627V_5C-y8xGIoSF6c5P9tEsu1cURX55EmFl5A4OAYb3xHJNYpueeWSsTMkyk60fg7ZA1ATr74B0QzEo1zKnjzZxQoZHYayWKlFmi-LSYic3lnDFgV35WEjveSzzPyOZQprTkDh5ee21QbJkER2lZWv5KqQge1ViOhi1UPWa1OjtJyAQGv86W17EQ2wA0_ESFUg3-kahwfhhr5_PxtWc_P698vQ-fFiXL0Qvra_-6erzJcvy4ghUanp5k2xUNkR-nC_tX3lLqvAqL3Ad_PqdDdOPcGdd0qHPSJBDf6lB1.IKEnNxngSfQRGEunXf6b8w",
+      "requestCallType": "POST",
+      "url": "https://fusion.preprod.zeta.in/api/v1/ifi/140793/transfers",
+      "requestID" : val,
+      "amount": {
+          "currency" : "INR",
+          "amount" : "100"
+      },
+      "transferCode": "ATLAS_P2M_AUTH",
+      "debitAccountID": "a27bce72-467e-463f-86cf-2e90b9baa3da",
+      "creditAccountID": this.shared.user.accountID,
+      "transferTime": 1574741608001,
+      "remarks": "AH-1 VBO  a/c",
+      "attributes": {}
+    }
+    this.showLoader();
+    this.authService.postData(data, 'hitZetaAPI').then(async (result) => {
+      await this.loading.dismiss();
+      console.log(result);
+      if (result['status']=='SUCCESS'){
+        this.shared.savescore(1);
+        this.nextpage(0);
+      }
+      else{
+        const alert = await this.alertCtrl.create({
+          header: 'Error',
+          message: 'Something went wrong. Try again later',
+          buttons: ['OK'],
+        });
+        await alert.present();
+      }
+    }, async (err) => {
+      await this.loading.dismiss();
       console.log(err);
       const alert = await this.alertCtrl.create({
         header: 'Error',
@@ -196,4 +275,13 @@ export class Level3Page implements OnInit {
       await alert.present();
     });
   }
+
+  async showLoader(){
+    this.loading = await this.loadingCtrl.create({
+        message: 'Loading..',
+    });
+    await this.loading.present();
+  }
+
+  
 }
